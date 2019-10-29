1c72a8837bbb2a4518dc5887b745fdfede063277
tdiallo <Brasory21>
2019-10-29 16:31:18 -0400

    init of the legacy Web app

---
+++ src/main/java/servlet/BackendServlet.java
@@ -0,0 +1,34 @@
+package servlet;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BackendServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+
+        /* Populating the DB if it is empty. */
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        /* Initialize some variables. */
+        String redirectedUrl = "/index.jsp";
+
+        getServletContext()
+                .getRequestDispatcher(redirectedUrl)
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // nothing.
+    }
+}
